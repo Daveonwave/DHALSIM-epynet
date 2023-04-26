e64f7f9f43ddd6434b63416181b3d88d6bc51ffd
Andrés F. Murillo <afmurillo@users.noreply.github.com>
2023-04-26 13:57:10 +0800

    Refactoring to include a setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='epynet',
       author_email='abel.heinsbroek@vitens.nl',
       license='Apache Licence 2.0',
       packages=['epynet'],
-      package_data={'epynet': ['core/lib/*']},
+      package_data={'epynet': ['epynet/lib/*']},
       install_requires=[
           'pandas'
       ],
